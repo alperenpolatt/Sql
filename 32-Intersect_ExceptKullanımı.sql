a158b496e84b32d9718c24c85bf6a1faa6cbd161
alperenpolatt <mustafaalperenpolat@gmail.com>
2025-05-16 15:38:38 +0300

    Add Intersect-Except

---
+++ 32-Intersect_ExceptKullanımı.sql
@@ -0,0 +1,9 @@
+--Intersect 2 sorgunun kesiþim yerlerini bulmak için kullanýlan sql komutudur 
+
+USE testDB
+GO
+SELECT CountryId FROM Persons
+INTERSECT
+SELECT Id FROM Country
+
+--Except 2 sorgunun farkýnda kullanýlýr.
